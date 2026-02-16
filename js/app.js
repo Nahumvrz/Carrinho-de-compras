d22cda091fb4638b9400414824544083115ab476
Nahum Carpio <nahum20x20@gmil.com>
2026-02-15 21:45:31 -0300

    Mudança do curso 'reemplazar condicional por uso do comando split para manipular o valor das opções'

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-let preco =[100 , 1400 , 5000];
-let precos;
 let calculoTotal;
 limpar();
 
@@ -7,17 +5,15 @@ limpar();
 function adicionar() {
     let quantidade = document.getElementById('quantidade').value;
     let opcoes = document.getElementById('produto').value;
-    if (opcoes === 'Fone de ouvido - R$100') {
-        opcoes = opcoes.replace('Fone de ouvido - R$100', 'Fone de ouvido');
-    } else if (opcoes === 'Celular - R$1400') {
-        opcoes = opcoes.replace('Celular - R$1400', 'Celular');
-    } else{
-        opcoes = opcoes.replace('Oculus VR - R$5000', 'Oculus VR');
-    }
+    let nomeProduto = opcoes.split(' -')[0];
+    let precoProduto = opcoes.split('R$')[1];
+    alert(nomeProduto);
+    alert(precoProduto);
+    alert(quantidade);
     
     precos = opcoes == 'Fone de ouvido' ? preco[0] : opcoes == 'Celular' ? preco[1] : preco[2];
     let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML += `<span class="texto-azul">${quantidade}x</span>${opcoes}<span class="texto-azul">$${precos}</span>`;
+    carrinho.innerHTML += `<span class="texto-azul">${quantidade}x</span>${opcoes}<span class="texto-azul">R$${precos}</span>`;
     
 }
 
@@ -27,8 +23,6 @@ function valorTotal() {
     let valorTotal = document.getElementById('valor-total').value;
     valorTotal.innerHTML = `Total: <span class="texto-azul" id="valor-total">${calculoTotal}</span>`;
 }
-
-valorTotal();
 
 
 
