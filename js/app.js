a52e2ae343ecb8cee5b996841c97ed899e27b1d6
Nahum Carpio <nahum20x20@gmil.com>
2026-02-15 23:40:37 -0300

    Mudança da calculo, e outras coisas

--- js/app.js
+++ js/app.js
@@ -1,34 +1,15 @@
-let calculoTotal;
-limpar();
-
-
 function adicionar() {
-    let quantidade = document.getElementById('quantidade').value;
-    let opcoes = document.getElementById('produto').value;
-    let nomeProduto = opcoes.split(' -')[0];
-    let precoProduto = opcoes.split('R$')[1];
+    let opcoes = document.getElementById('produto');
+    let nomeProduto = opcoes.split('-')[0];
+    let valorUnitario = opcoes.split('R$')[1];
+    let quantidade = document.getElementById('quantidade');
     alert(nomeProduto);
-    alert(precoProduto);
-    alert(quantidade);
-    
-    precos = opcoes == 'Fone de ouvido' ? preco[0] : opcoes == 'Celular' ? preco[1] : preco[2];
-    let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML += `<span class="texto-azul">${quantidade}x</span>${opcoes}<span class="texto-azul">R$${precos}</span>`;
-    
+    alert(valorUnitario);
+    alert(quantidade.value);
+    let preco = quantidade.value * valorUnitario;
+    alert(preco);
 }
-
-
-function valorTotal() {
-    calculoTotal = calculoTotal + (quantidade * precos);
-    let valorTotal = document.getElementById('valor-total').value;
-    valorTotal.innerHTML = `Total: <span class="texto-azul" id="valor-total">${calculoTotal}</span>`;
-}
-
 
 
 function limpar() {
-    let limpar = document.getElementById('lista-produtos');
-    limpar.innerHTML = '';
-    valorTotal = document.getElementById('valor-total');
-    valorTotal.innerHTML = `<span class="texto-azul" id="valor-total">0</span>`;
 }
