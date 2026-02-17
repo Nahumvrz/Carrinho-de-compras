41b884254e45e7f6258e958b9735849ffb885e68
Nahum Carpio <nahum20x20@gmil.com>
2026-02-16 23:36:10 -0300

    Implementação de validações com if else para verificar produto e quantidade validos

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
 let valorGeral;
 limpar();
 
+//Acrescentar validações no carrinho
+// 1. Se tiver selecionado um produto valido
+// 2. Se tiver inserido uma quantidade valida
+// 3. Se a entrada não for valida, exibir uma mensagem de erro
+// 4. A mensagem deve ser exibida no tala mesmo, não em um alert
+
 function adicionar() {
     let opcoes = document.getElementById('produto').value;
     let nomeProduto = opcoes.split('-')[0];
     let valorUnitario = opcoes.split('R$')[1];
     let quantidade = document.getElementById('quantidade').value;
+
+    if ((quantidade < 0 ) || (quantidade > 100)) {
+        let mensagemErro = document.querySelector('h1');
+        mensagemErro.textContent = 'Produto ou quantidade indisponíveis, selecione um produto disponível e insira uma quantidade entre 1 e 100';
+        return;
+    }
+
+    document.querySelector('h1').innerHTML = 'Carrinho de <span class="texto-azul">compras</span>';
     let preco = quantidade * valorUnitario;
     let carrinho = document.getElementById('lista-produtos');
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
